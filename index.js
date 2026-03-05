447183f36c0b7fe89854024256c8e46dabe16510
aswathi-48 <aswathik482@gmail.com>
2026-03-05 15:01:43 +0530

    changes

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ const app = express()
 import dotenv from 'dotenv'
 import connection from './config/db.js'
 import userRoutes from './routes/userRoute.js'
+import blogRoutes from './routes/blogRoute.js'
 dotenv.config()
 app.use(express.json())
 connection()
 const port = process.env.PORT
 
 app.use('/user',userRoutes)
+app.use('/blog',blogRoutes)
 
 app.listen(port,()=>{
 console.log(`server running on port ${port}`);
