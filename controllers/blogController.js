91af3b64eb399bf2670c5c79b45422e6c5c91fc2
aswathi-48 <aswathik482@gmail.com>
2026-03-05 15:29:40 +0530

    filter and serach added

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -22,16 +22,50 @@ try{
 }
 }
 
-export const getBlog  = async (req,res)=>{
-    try{
-        const listBlog = await Blog.find()
-        res.status(200).json({
-            status: true,
-            message: "success",
-            data: listBlog
-        })
-    }catch(err){
-        console.log(err);
+// export const getBlog  = async (req,res)=>{
+//     try{
+//         const listBlog = await Blog.find()
+//         res.status(200).json({
+//             status: true,
+//             message: "success",
+//             data: listBlog
+//         })
+//     }catch(err){
+//         console.log(err);
         
+//     }
+// }
+
+
+// getBlog
+export const getBlog = async (req, res) => {
+  try {
+    const { title, category } = req.query;
+
+    const filter= {};
+
+    if (title) {
+      filter.title = { $regex: title, $options: "i" };
     }
-}
+
+    if (category) {
+      filter.category = { $regex: category, $options: "i" };
+    }
+
+    const listBlog = await Blog.find(filter)
+    //   .populate("user", "name email")
+      .sort({ createdAt: -1 });
+
+    res.status(200).json({
+      status: true,
+      message: "Blogs fetched successfully",
+      data: listBlog,
+    });
+  } catch (error) {
+    console.error("Get Blog Error:", error);
+    res.status(500).json({
+      status: false,
+      message: "Server error",
+    });
+  }
+};
