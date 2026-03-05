5423dd1eb5b421e4d33f58df4627865b82468846
aswathi-48 <aswathik482@gmail.com>
2026-03-05 10:14:08 +0530

    register route completed

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ import express from 'express'
 const app = express()
 import dotenv from 'dotenv'
 import connection from './config/db.js'
+import userRoutes from './routes/userRoute.js'
 dotenv.config()
 app.use(express.json())
 connection()
 const port = process.env.PORT
+
+app.use('/user',userRoutes)
 
 app.listen(port,()=>{
 console.log(`server running on port ${port}`);
