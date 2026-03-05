5db02beafd7a1e5b517d444d533abc88f626c1a9
aswathi-48 <aswathik482@gmail.com>
2026-03-05 15:19:26 +0530

    get blog completed

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { addBlog } from "../controllers/blogController.js";
+import { addBlog, getBlog } from "../controllers/blogController.js";
 
 const router = Router()
 
 router.post("/add",addBlog)
+router.get("/get",getBlog)
 
 export default router
