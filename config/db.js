72e7fefbdf34fd806c41dd2e9a71bad2630c096e
aswathi-48 <aswathik482@gmail.com>
2026-03-05 09:37:27 +0530

    mongodb connected

---
+++ config/db.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose"
+
+const connection =()=>{
+    mongoose.connect(process.env.MONGO_URL).then(()=>{
+        console.log("Mongodb connected");
+        
+    })
+}
+
+export default connection
--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from 'express'
 const app = express()
+import dotenv from 'dotenv'
+import connection from './config/db.js'
+dotenv.config()
 
-
+connection()
 const port = 3000
 
 app.listen(port,()=>{
