76600b0387bb17de6357f8cc80374fc74c769b80
aswathi-48 <aswathik482@gmail.com>
2026-03-05 15:44:21 +0530

    authcheck created

---
+++ middlewares/authCheck.js
@@ -0,0 +1,33 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/userSchema');
+
+//authorization
+ const authCheck = async (req, res, next) => {
+    if ( req.method === "OPTIONS" ) {
+        return next()
+    } else {
+        try {
+            const token = req.headers.authorization.split(' ') [1]
+            if (!token) {
+                console.log("authentication failed!");   
+            } else {    
+         
+                const decodedToken = jwt.verify(token,process.env.JWT_SECRET)
+                const validUser = await User.findOne({ _id : decodedToken.userId })
+                console.log(validUser);
+                
+                if (! validUser) {
+                    console.log("Invalid credentials!");
+                } else {
+                    req.userDetails = { userId : decodedToken.userId, }
+                    next()
+                }
+
+            }
+        } catch (error) {
+            console.log("Authentication failed!");
+        }
+    }
+
+}
+module.exports = authCheck;
