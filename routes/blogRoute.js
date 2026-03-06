e5e9749d247fa804b030e626e37a83caa08aad57
aswathi-48 <aswathik482@gmail.com>
2026-03-06 11:19:26 +0530

    changes

--- routes/blogRoute.js
+++ routes/blogRoute.js
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import { addBlog, deleteBlog, getBlog, getSingleBlog, updateBlog } from "../controllers/blogController.js";
 import authCheck from "../middlewares/authCheck.js";
+import upload from "../middlewares/upload.js";
 
 const router = Router()
 
 router.use(authCheck)
-router.post("/add",addBlog)
-router.get("/get",getBlog)
-router.get('/single',getSingleBlog)
-router.patch('/update',updateBlog)
-router.delete('/delete',deleteBlog)
+router.post("/add", upload.single('thumbnail'), addBlog)
+router.get("/get", getBlog)
+router.get('/single', getSingleBlog)
+router.patch('/update', upload.single('thumbnail'), updateBlog)
+router.delete('/delete', deleteBlog)
 
 export default router
